acfecee184009f52d4e4064d095c326ca8ff948f
Jerodev <jeroen@deviaene.eu>
2019-03-22 09:47:58 +0100

    Add product search responders

---
+++ app/Irc/Responders/ProductResponder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Irc\Responders;
+
+use App\Irc\Response;
+use Jerodev\PhpIrcClient\IrcChannel;
+
+/**
+ *  Search for a product in several online stores
+ *
+ *  `!amazon {search string}`
+ */
+class ProductResponder extends Responder
+{
+    public function handlePrivmsg(string $from, IrcChannel $to, string $message, bool $respond = true): ?Response
+    {
+        if ($respond === false || strpos($message, ' ') === false) {
+            return null;
+        }
+
+        $data = explode(' ', $message, 2);
+        if (empty(trim($data[1]))) {
+            return null;
+        }
+
+        switch ($data[0])
+        {
+            case '!amazon':
+                return $this->amazonSearch($data[1]);
+                break;
+        }
+
+        return null;
+    }
+
+    private function amazonSearch(string $search): ?Response
+    {
+        $search = urlencode($search);
+        return new Response("https://www.amazon.de/s/?tag=tabfin0b-21&field-keywords=$search");
+    }
+}
---
+++ tests/ResponderTests/ProductResponderTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\ResponderTests;
+
+use App\Irc\Responders\ProductResponder;
+use Jerodev\PhpIrcClient\IrcChannel;
+use Tests\TestCase;
+
+class ProductResponderTest extends TestCase
+{
+    public function testEmptyPayload()
+    {
+        $responder = new ProductResponder();
+        $response = $responder->handlePrivmsg('foo', new IrcChannel('#bar'), '!amazon  ');
+
+        $this->assertNull($response);
+    }
+
+    public function testAmazonSearch()
+    {
+        $responder = new ProductResponder();
+        $response = $responder->handlePrivmsg('foo', new IrcChannel('#bar'), '!amazon Terraforming Mars');
+
+        $this->assertNotNull($response);
+        $this->assertContains('amazon.de', $response->getMessage());
+        $this->assertContains('&field-keywords=Terraforming+Mars', $response->getMessage());
+    }
+}
